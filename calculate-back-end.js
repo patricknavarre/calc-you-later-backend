1a50cfa9eb5a7119f6927fa51e56ae3df52f6a0e
Patrick Navarre <patrick.navarre@codeimmersives.com>
2020-10-22 10:22:07 -0400

    calculate-backend-funtion-completed

--- calculate-back-end.js
+++ calculate-back-end.js
@@ -1,9 +1,33 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const calculate = function(num1, num2, calculation) {
+  // Number function converts the object argument to a number that represents the object's value.  
+  num1 = Number(num1);
+  num2 = Number(num2);
 
+  // assigning multiple 'string' tasks to each calculation
+  const addition = calculation === '+' || calculation === 'plus' || calculation === 'added to';
+  const subtraction = calculation === '-' || calculation === 'minus' || calculation === 'subtracted from';
+  const multiplication = calculation.toLowerCase() === 'x' || calculation === 'times' || calculation === 'multiplied by';
+  const division = calculation === '/' || calculation === 'divided by';
+  const modulus = calculation === '%' || calculation === 'modulus' || calculation === 'mod';
+// if/else through all possible calculations until 'Sorry...' which will close the function.
+  if (addition) {
+    return num1 + num2;
+  } else if (subtraction) {
+    return num1 - num2;
+  } else if (multiplication) {
+    return num1 * num2;
+  } else if (division) {
+    return num1 / num2;
+  } else if (modulus) {
+    return num1 % num2;
+  } else {
+    return `Sorry, that's not a mathematical operation!`
+  }
+}
 
- 
 
 /********************************************
  * CODE DOWN HERE IS FOR INTERNAL USE ONLY. *
